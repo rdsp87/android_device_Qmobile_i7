3384eabc79d0bd8978773f4f75c12809e905d3a7
Hassams <hassam363@gmail.com>
2014-12-22 15:49:11 +0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,14 +1,14 @@
 # Correct bootanimation size for the screen
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/google/sprout/full_sprout.mk)
+$(call inherit-product, device/google/i7/full_i7.mk)
 
-PRODUCT_NAME := cm_sprout
-PRODUCT_DEVICE :=sprout
-PRODUCT_BRAND := google
-PRODUCT_MANUFACTURER := Google
-PRODUCT_MODEL := Android One
+PRODUCT_NAME := cm_i7
+PRODUCT_DEVICE :=i7
+PRODUCT_BRAND := QMobile
+PRODUCT_MANUFACTURER := QMobile
+PRODUCT_MODEL := Noir

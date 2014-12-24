304c62d7d9d4d9f8bca8c1d19a7b7b95c40e89ad
Hassams <hassam363@gmail.com>
2014-12-24 21:04:43 +0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/QMobile/i7/full_i7.mk)
 
 PRODUCT_NAME := cm_i7
-PRODUCT_DEVICE :=Noir
+PRODUCT_DEVICE :=i7
 PRODUCT_BRAND := QMobile
 PRODUCT_MANUFACTURER := QMobile
 PRODUCT_MODEL := i7
